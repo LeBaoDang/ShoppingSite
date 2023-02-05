d720b871e876a35c0d9552eff2805b6ca8e0a415
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2023-02-06 00:15:27 +0700

    update

--- src/main/java/com/store/util/Constant.java
+++ src/main/java/com/store/util/Constant.java
@@ -1,8 +1,8 @@
 package com.store.util;
 
 public class Constant {
-    public static final int PAGE_PRODUCT_MAX_SIZE = 6;
-    public static final int PAGE_PRODUCT_CATEGORY_MAX_SIZE = 6;
+    public static final int PAGE_PRODUCT_MAX_SIZE = 8;
+    public static final int PAGE_PRODUCT_CATEGORY_MAX_SIZE = 8;
     
     // không cho new hàm tạo 
  	// chỉ cho sử dụng qua class không cho new qua đối tượng
