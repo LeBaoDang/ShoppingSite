17ff50967a4acba3b323ebfe426348f0e04036f0
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2023-02-10 00:16:06 +0700

    update

--- src/main/java/com/store/dto/requestdto/AccountRequestDto.java
+++ src/main/java/com/store/dto/requestdto/AccountRequestDto.java
@@ -2,18 +2,26 @@ package com.store.dto.requestdto;
 
 import java.util.List;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class AccountRequestDto {
 	
+	@NotBlank(message = "Vui lòng nhập username")
 	private String username;
 	
+	@NotBlank(message = "Vui lòng nhập password")
+	private String password;
+	
+	@NotBlank(message = "Vui lòng nhập họ và tên")
 	private String fullname;
 	
+	@NotBlank(message = "Vui lòng nhập email")
+	@Email(message = "không đúng định dạng email")
 	private String email;
-	
-	private String password;
 	
 	private String photo;
 	
