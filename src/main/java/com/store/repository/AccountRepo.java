de572a7f3ac9cd54a7785bc02d014a322494be4a
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2023-04-07 18:59:16 +0700

    update

--- src/main/java/com/store/repository/AccountRepo.java
+++ src/main/java/com/store/repository/AccountRepo.java
@@ -16,9 +16,4 @@ public interface AccountRepo extends JpaRepository<Account, String> {
 	@Query("SELECT DISTINCT ar.account FROM Authority ar WHERE ar.role.id IN ('DIRE', 'STAF')")
 	List<Account> getAdministrators();
 
-//	@Query(" SELECT r.name FROM Authority o INNER JOIN Role r ON " +
-//			"o.role = r.id WHERE o.account.username =: username")
-//	@Query( value = "select Name from Authorities inner join roles on Authorities.RoleId = roles.Id where Username = ?1 ", nativeQuery = true)
-//	List<Role> RoleFindAll(String uername);
-
 }
