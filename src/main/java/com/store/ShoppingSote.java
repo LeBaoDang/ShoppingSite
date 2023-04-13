435678b537f5371900da599db3bab6ba3b4fa7fd
Lê Bảo Đăng <85282838+LeBaoDang@users.noreply.github.com>
2023-04-13 21:37:30 +0700

    update

--- src/main/java/com/store/ShoppingSote.java
+++ src/main/java/com/store/ShoppingSote.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShoppingSote {
-
     public static void main(String[] args) {
         SpringApplication.run(ShoppingSote.class, args);
     }
-
 }
